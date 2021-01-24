675abb3d321d3eeb0dc256d3488e95292cdbaac2
Mirza <muhammedmirzakilic@gmail.com>
2021-01-24 20:30:16 +0300

    running instruction ability was implemented

--- rover/Rover.cs
+++ rover/Rover.cs
@@ -18,6 +18,33 @@ namespace rover
             MaxY = maxY;
         }
 
+        public void RunInstruction(Instruction instruction)
+        {
+            if (instruction == Instruction.L) ChangeHeading(1);
+            else if (instruction == Instruction.R) ChangeHeading(-1);
+            else if (instruction == Instruction.M) Move();
+            else throw new Exception("Unrecognized instruction!");
+        }
+
+        private void Move()
+        {
+            if (Heading == Direction.E) X++;
+            else if (Heading == Direction.N) Y++;
+            else if (Heading == Direction.W) X--;
+            else if (Heading == Direction.S) Y--;
+
+            //prevent rover from getting out of range
+            if (X < 0) X = 0;
+            if (X > MaxX) X = MaxX;
+            if (Y < 0) Y = 0;
+            if (Y > MaxY) Y = MaxY;
+        }
+
+        private void ChangeHeading(int rotateTo)
+        {
+            Heading = (Direction)((int)(Heading + (byte)rotateTo) % 4);
+        }
+
         public override string ToString()
         {
             return $"{X} {Y} {Heading}";
