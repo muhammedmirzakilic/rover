5b2623786ae46210e559106bd466015533ed52f8
Mirza <muhammedmirzakilic@gmail.com>
2021-01-24 20:07:55 +0300

    rover class implemented

---
+++ rover/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace rover
+{
+    public enum Instruction : byte
+    {
+        L,
+        R,
+        M
+    }
+
+    public enum Direction : byte
+    {
+        S,
+        E,
+        N,
+        W
+    }
+}
---
+++ rover/Rover.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace rover
+{
+    public class Rover
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int MaxX { get; private set; }
+        public int MaxY { get; private set; }
+        public Direction Heading { get; private set; }
+
+        public Rover(int initialX, int initialY, Direction heading, int maxX, int maxY)
+        {
+            X = initialX;
+            Y = initialY;
+            Heading = heading;
+            MaxX = maxX;
+            MaxY = maxY;
+        }
+
+        public override string ToString()
+        {
+            return $"{X} {Y} {Heading}";
+        }
+    }
+}
